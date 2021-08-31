9aebb5b6230882a0f513a4e3975929516462e2b2
Daniel Obraczka <obraczka@informatik.uni-leipzig.de>
2021-08-31 17:39:27 +0200

    Missed file for improved rdf handling

--- forayer/io/from_to_rdf.py
+++ forayer/io/from_to_rdf.py
@@ -1,36 +1,63 @@
 """Read and write semantic web sources."""
 import logging
 from collections import defaultdict
-from typing import Callable
+from typing import Callable, Set
 
+import rdflib
 from forayer.knowledge_graph import KG
 from rdflib import Graph
 from rdflib.term import Literal
 from tqdm import tqdm
 
 
-def cast_to_python_type(lit):
+def cast_to_python_type(lit: rdflib.term.Literal):
+    """Casts a literal to the respective python type.
+
+    Parameters
+    ----------
+    lit : rdflib.term.Literal
+       The literal that is to be cast.
+
+    Returns
+    -------
+    Any
+        The literal as the respective python object
+    """
     if lit.datatype is not None and "langString" in lit.datatype:
         # lang strings are not automatically cast
         return str(lit)
     return lit.toPython()
 
 
-def add_multi_value(prev, new):
+def add_multi_value(prev, new) -> Set:
+    """Add a  value to a set or create a new set with prev and new.
+
+    Parameters
+    ----------
+    prev
+        Existing value.
+    new
+        New value, that should be added.
+
+    Returns
+    -------
+    Set
+        Set containing the previous and new elements.
+    """
     if not isinstance(prev, set):
         prev = {prev}
     prev.add(new)
     return prev
 
 
-def load_from_triples(
+def load_from_rdf(
     in_path: str,
     literal_cleaning_func: Callable = None,
     format: str = None,
     kg_name: str = None,
     multi_value: Callable = None,
 ) -> KG:
-    """Create knowledge graph object.
+    """Create knowledge graph object from rdf source.
 
     Parameters
     ----------
@@ -71,3 +98,26 @@ def load_from_triples(
         else:
             rel[str(s)][str(o)] = str(p)
     return KG(entities=entities, rel=rel, name=kg_name)
+
+
+def write_to_rdf(
+    kg: KG, out_path: str, format: str, prefix: str = "", attr_mapping: dict = None
+):
+    """Writes the forayer knowledge graph to a rdf serialization format.
+
+    Parameters
+    ----------
+    kg : KG
+        The knowledge graph that will be serialized.
+    out_path : str
+        The path where it should be serialized to.
+    format : str
+        The desired rdf format.
+    prefix : str
+        Prefix for the entities in the graph.
+    attr_mapping : dict
+       Mapping of attribute names.
+    """
+    kg.to_rdflib(prefix=prefix, attr_mapping=attr_mapping).serialize(
+        destination=out_path, format=format
+    )
