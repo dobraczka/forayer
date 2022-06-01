f6f0e941f2379a37e62142d87d4f2ec124de0098
Daniel Obraczka <obraczka@informatik.uni-leipzig.de>
2022-06-01 14:22:01 +0200

    fix dependencies and move to nox

---
+++ noxfile.py
@@ -0,0 +1,44 @@
+from nox_poetry import Session, session
+
+
+@session()
+def tests(session: Session) -> None:
+    args = session.posargs or ["--cov=forayer/", "--cov-report=xml", "tests/"]
+    session.install(".[all]")
+    session.install("pytest")
+    session.install("pytest-cov")
+    session.run("pytest", *args)
+
+
+locations = ["forayer", "tests", "noxfile.py"]
+
+
+@session()
+def lint(session: Session) -> None:
+    args = session.posargs or locations
+    session.install(
+        "pyproject-flake8",
+        "flake8-eradicate",
+        "flake8-isort",
+        "flake8-debugger",
+        "flake8-comprehensions",
+        "flake8-print",
+    )
+    session.run("pflake8", *args)
+
+
+@session()
+def type_checking(session: Session) -> None:
+    args = session.posargs or locations
+    session.install("mypy")
+    session.run("mypy", "--ignore-missing-imports", *args)
+
+
+@session()
+def build_docs(session: Session) -> None:
+    session.install(".")
+    session.install("sphinx")
+    session.install("insegel")
+    session.cd("docs")
+    session.run("make", "clean", external=True)
+    session.run("make", "html", external=True)
